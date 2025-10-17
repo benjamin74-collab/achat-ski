3b8b09c0b4689660a480491234226283286826bc
Ton Nom <tonemail@example.com>
2025-10-17 22:52:00 +0200

    correction Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -125,7 +125,7 @@ export default function Header() {
                 </button>
               </>
             ) : (
-              <button onClick={() => signIn()} className="btn">
+              <button onClick={() => signIn(undefined, { callbackUrl: "/admin" })} className="btn">
                 Se connecter
               </button>
             )}
